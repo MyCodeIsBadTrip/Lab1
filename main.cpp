37e4aa0488774a74683430c8773778dcf91113e7
MyCodeIsBadTrip <66428126+MyCodeIsBadTrip@users.noreply.github.com>
2025-05-20 01:19:02 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,29 @@
-#include <string>
+#include <iostream>
+#include <cmath>
 
-int main() {
-    std::cout << "Enter string: " << std::endl;
-    std::string str1;
-    std::getline(std::cin, str1);
-    char symbol;
-    int counter = 0;
-
-    for (int i = 0; i < size(str1) + 1; i++) {
-        symbol = str1[i];
-        if ((symbol == ' ' || symbol == ',' || symbol == '.' || i == size(str1)) && ( i != 0) && (str1[i-1] != ' ' && str1[i-1] != ',' && str1[i-1] != '.')) {
-            counter++;
+bool isPrime(int n) {
+    if (n <= 1) return false;
+    if (n == 2) return true;
+    if (n % 2 == 0) return false;
+    
+    for (int i = 3; i <= std::sqrt(n); i += 2) {
+        if (n % i == 0) {
+            return false;
         }
     }
+    return true;
+}
 
-    std::cout << "words count: " << counter << std::endl;
+int main() {
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+
+    if (isPrime(number)) {
+        std::cout << number << " is a prime number." << std::endl;
+    } else {
+        std::cout << number << " is not a prime number." << std::endl;
+    }
+    
     return 0;
 }
